be20f4a3f223bec98fbec2f88f6518a538b36512
Peter Gusev <peter@remap.ucla.edu>
2021-09-14 21:28:41 -0800

    added cnl-cpp-config.h (Windows)

---
+++ VisualStudio/cnl-cpp-config.h
@@ -0,0 +1,277 @@
+#ifndef _INCLUDE_CNL_CPP_CNL_CPP_CONFIG_H
+#define _INCLUDE_CNL_CPP_CNL_CPP_CONFIG_H 1
+ 
+/* include/cnl-cpp/cnl-cpp-config.h. Generated automatically at end of configure. */
+/* include/config.h.  Generated from config.h.in by configure.  */
+/* include/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* define if the Boost library is available */
+#ifndef CNL_CPP_HAVE_BOOST
+#define CNL_CPP_HAVE_BOOST /**/
+#endif
+
+/* define if the Boost::ASIO library is available */
+#ifndef CNL_CPP_HAVE_BOOST_ASIO
+#define CNL_CPP_HAVE_BOOST_ASIO /**/
+#endif
+
+/* define if the compiler supports basic C++11 syntax */
+#ifndef CNL_CPP_HAVE_CXX11
+#define CNL_CPP_HAVE_CXX11 1
+#endif
+
+/* define if the compiler supports basic C++14 syntax */
+#ifndef CNL_CPP_HAVE_CXX14
+#define CNL_CPP_HAVE_CXX14 1
+#endif
+
+/* define if the compiler supports basic C++17 syntax */
+#ifndef CNL_CPP_HAVE_CXX17
+#define CNL_CPP_HAVE_CXX17 1
+#endif
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#ifndef CNL_CPP_HAVE_DLFCN_H
+#define CNL_CPP_HAVE_DLFCN_H 1
+#endif
+
+/* Define to 1 if you have the `gettimeofday' function. */
+#ifndef CNL_CPP_HAVE_GETTIMEOFDAY
+#define CNL_CPP_HAVE_GETTIMEOFDAY 1
+#endif
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#ifndef CNL_CPP_HAVE_INTTYPES_H
+#define CNL_CPP_HAVE_INTTYPES_H 1
+#endif
+
+/* Define to 1 if you have the `crypto' library (-lcrypto). */
+#ifndef CNL_CPP_HAVE_LIBCRYPTO
+#define CNL_CPP_HAVE_LIBCRYPTO 1
+#endif
+
+/* Define to 1 if you have the `log4cxx' library (-llog4cxx). */
+#ifndef CNL_CPP_HAVE_LIBLOG4CXX
+#define CNL_CPP_HAVE_LIBLOG4CXX 0
+#endif
+
+/* Define to 1 if you have the `ndn-ind' library (-lndn-ind). */
+#ifndef CNL_CPP_HAVE_LIBNDN_IND
+#define CNL_CPP_HAVE_LIBNDN_IND 1
+#endif
+
+/* Define to 1 if you have the `ndn-ind-tools' library (-lndn-ind-tools). */
+#ifndef CNL_CPP_HAVE_LIBNDN_IND_TOOLS
+#define CNL_CPP_HAVE_LIBNDN_IND_TOOLS 1
+#endif
+
+/* Define to 1 if you have the `protobuf' library (-lprotobuf). */
+#ifndef CNL_CPP_HAVE_LIBPROTOBUF
+#define CNL_CPP_HAVE_LIBPROTOBUF 1
+#endif
+
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+#ifndef CNL_CPP_HAVE_LIBPTHREAD
+#define CNL_CPP_HAVE_LIBPTHREAD 1
+#endif
+
+/* Define to 1 if you have the `sqlite3' library (-lsqlite3). */
+#ifndef CNL_CPP_HAVE_LIBSQLITE3
+#define CNL_CPP_HAVE_LIBSQLITE3 0
+#endif
+
+/* Define to 1 if you have the `memcmp' function. */
+#ifndef CNL_CPP_HAVE_MEMCMP
+#define CNL_CPP_HAVE_MEMCMP 1
+#endif
+
+/* Define to 1 if you have the `memcpy' function. */
+#ifndef CNL_CPP_HAVE_MEMCPY
+#define CNL_CPP_HAVE_MEMCPY 1
+#endif
+
+/* Define to 1 if you have the `memset' function. */
+#ifndef CNL_CPP_HAVE_MEMSET
+#define CNL_CPP_HAVE_MEMSET 1
+#endif
+
+/* 1 if have Google Protobuf. */
+#ifndef CNL_CPP_HAVE_PROTOBUF
+#define CNL_CPP_HAVE_PROTOBUF 1
+#endif
+
+/* Define if you have POSIX threads libraries and header files. */
+/* #undef HAVE_PTHREAD */
+
+/* Define to 1 if you have the `round' function. */
+#ifndef CNL_CPP_HAVE_ROUND
+#define CNL_CPP_HAVE_ROUND 1
+#endif
+
+/* Define if you have sqlite3 library and header files. */
+/* #undef HAVE_SQLITE3 */
+
+/* Define to 1 if you have the `sscanf' function. */
+#ifndef CNL_CPP_HAVE_SSCANF
+#define CNL_CPP_HAVE_SSCANF 1
+#endif
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#ifndef CNL_CPP_HAVE_STDINT_H
+#define CNL_CPP_HAVE_STDINT_H 1
+#endif
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#ifndef CNL_CPP_HAVE_STDIO_H
+#define CNL_CPP_HAVE_STDIO_H 1
+#endif
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#ifndef CNL_CPP_HAVE_STDLIB_H
+#define CNL_CPP_HAVE_STDLIB_H 1
+#endif
+
+/* Define to 1 if you have the `strchr' function. */
+#ifndef CNL_CPP_HAVE_STRCHR
+#define CNL_CPP_HAVE_STRCHR 1
+#endif
+
+/* Define to 1 if you have the <strings.h> header file. */
+#ifndef CNL_CPP_HAVE_STRINGS_H
+#define CNL_CPP_HAVE_STRINGS_H 1
+#endif
+
+/* Define to 1 if you have the <string.h> header file. */
+#ifndef CNL_CPP_HAVE_STRING_H
+#define CNL_CPP_HAVE_STRING_H 1
+#endif
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#ifndef CNL_CPP_HAVE_SYS_STAT_H
+#define CNL_CPP_HAVE_SYS_STAT_H 1
+#endif
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#ifndef CNL_CPP_HAVE_SYS_TIME_H
+#define CNL_CPP_HAVE_SYS_TIME_H 1
+#endif
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#ifndef CNL_CPP_HAVE_SYS_TYPES_H
+#define CNL_CPP_HAVE_SYS_TYPES_H 1
+#endif
+
+/* Define to 1 if you have the <time.h> header file. */
+#ifndef CNL_CPP_HAVE_TIME_H
+#define CNL_CPP_HAVE_TIME_H 1
+#endif
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#ifndef CNL_CPP_HAVE_UNISTD_H
+#define CNL_CPP_HAVE_UNISTD_H 0
+#endif
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#ifndef CNL_CPP_LT_OBJDIR
+#define CNL_CPP_LT_OBJDIR ".libs/"
+#endif
+
+/* Define to the address where bug reports for this package should be sent. */
+#ifndef CNL_CPP_PACKAGE_BUGREPORT
+#define CNL_CPP_PACKAGE_BUGREPORT "ndn-lib@lists.cs.ucla.edu"
+#endif
+
+/* Define to the full name of this package. */
+#ifndef CNL_CPP_PACKAGE_NAME
+#define CNL_CPP_PACKAGE_NAME "cnl-cpp"
+#endif
+
+/* Define to the full name and version of this package. */
+#ifndef CNL_CPP_PACKAGE_STRING
+#define CNL_CPP_PACKAGE_STRING "cnl-cpp 0.1"
+#endif
+
+/* Define to the one symbol short name of this package. */
+#ifndef CNL_CPP_PACKAGE_TARNAME
+#define CNL_CPP_PACKAGE_TARNAME "cnl-cpp"
+#endif
+
+/* Define to the home page for this package. */
+#ifndef CNL_CPP_PACKAGE_URL
+#define CNL_CPP_PACKAGE_URL "https://github.com/named-data/cnl-cpp"
+#endif
+
+/* Define to the version of this package. */
+#ifndef CNL_CPP_PACKAGE_VERSION
+#define CNL_CPP_PACKAGE_VERSION "0.1"
+#endif
+
+/* Define to the necessary symbol if this constant uses a non-standard name on
+   your system. */
+/* #undef PTHREAD_CREATE_JOINABLE */
+
+/* Define to 1 if all of the C90 standard headers exist (not just the ones
+   required in a freestanding environment). This macro is provided for
+   backward compatibility; new code need not use it. */
+#ifndef CNL_CPP_STDC_HEADERS
+#define CNL_CPP_STDC_HEADERS 1
+#endif
+
+/* Define for Solaris 2.5.1 so the uint32_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT32_T */
+
+/* Define for Solaris 2.5.1 so the uint64_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT64_T */
+
+/* Define for Solaris 2.5.1 so the uint8_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT8_T */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to the type of a signed integer type of width exactly 16 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int16_t */
+
+/* Define to the type of a signed integer type of width exactly 32 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int32_t */
+
+/* Define to the type of a signed integer type of width exactly 64 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int64_t */
+
+/* Define to the type of a signed integer type of width exactly 8 bits if such
+   a type exists and the standard includes do not define it. */
+/* #undef int8_t */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* Define to the type of an unsigned integer type of width exactly 16 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint16_t */
+
+/* Define to the type of an unsigned integer type of width exactly 32 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint32_t */
+
+/* Define to the type of an unsigned integer type of width exactly 64 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint64_t */
+
+/* Define to the type of an unsigned integer type of width exactly 8 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint8_t */
+ 
+/* once: _INCLUDE_CNL_CPP_CNL_CPP_CONFIG_H */
+#endif
